ed6d5f99204bcc934199a2f2e3f81801c408d5c5
Bhaskar001 <bhaskarjoshi900@gmail.com>
2026-03-13 12:31:37 +0530

    Branding: Rebrand backend to LogDay

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MONGODB_URL = os.getenv("MONGODB_URL")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "officeflow")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "logday")
 
 client = motor.motor_asyncio.AsyncIOMotorClient(
     MONGODB_URL,
